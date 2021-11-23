3eaf86f2fe8347e87e726ec18c8ec675a5e8c6c9
livi <scholtz.ice@gmail.com>
2021-11-23 13:51:55 +0200

    Corrected location for Google Maps element.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-console.log("javascript!!");
-
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
+    // The location of Brackenhurst
+    const brackenhurst = { lat: -26.315527180839673, lng: 28.099590713784654 };
+    // The map, centered at Brackenhurst
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru,
+        zoom: 12,
+        center: brackenhurst,
     });
-    // The marker, positioned at Uluru
+    // The marker, positioned at Brackenhurst
     const marker = new google.maps.Marker({
-        position: uluru,
+        position: brackenhurst,
         map: map,
     });
 }
